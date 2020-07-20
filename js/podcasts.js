fc9a7f1830b67ad7a2adab388b85007f3649b855
Paulo W. A. Ferreira <pauloweskley.dev@gmail.com>
2020-07-20 12:00:29 -0300

    Primeiro Commit, aplicação funcionando.

---
+++ js/podcasts.js
@@ -0,0 +1,80 @@
+var realPodcasts = [
+  {
+    frequency: '88.7',
+    title: 'Fala, Dev',
+    description:
+      'A gente passa a maior parte do tempo escrevendo código. Agora chegou o momento de falar sobre isso. Nesse podcast três programadores se reúnem para discutir o universo da programação.',
+    img: '../img/1.jpg',
+    url:'https://d3ctxlq1ktw2nl.cloudfront.net/staging/2020-05-04/8ec0d46bc40644158c232a93b8b6f2a9.m4a',
+  },
+  {
+    frequency: '90.1',
+    title: 'Full Stack Radio',
+    description:
+      'Um podcast para desenvolvedores interessados ​​em criar ótimos produtos de software. A cada episódio, Adam Wathan é acompanhado por um convidado para conversar sobre tudo, desde design de produto e experiência do usuário até teste de unidade e administração do sistema.',
+    img:'../img/2.jpg',
+    url:'https://cdn.simplecast.com/audio/05bd32/05bd32de-6cd4-40f6-b3bd-0bdf6750dd58/1f35559c-cd35-4c9e-8caa-0c30e738dd34/142_tc.mp3',
+  },
+  {
+    frequency: '92.2',
+    title: 'CodeNewbie',
+    description:'Histórias de pessoas em sua jornada de codificação',
+    img: '../img/3.png',
+    url: 'https://content.blubrry.com/codenewbie/CN_DevDiscuss_S1E1_NameChange.mp3',
+  },
+  {
+    frequency: '93.9',
+    title: 'Syntax',
+    description:
+      'Os desenvolvedores do Full Stack Wes Bos e Scott Tolinski se aprofundam nos tópicos de desenvolvimento da web, explicando como eles funcionam e falando sobre suas próprias experiências',
+    img: '../img/4.png',
+    url: 'https://traffic.libsyn.com/syntax/Syntax266.mp3',
+  },
+  {
+    frequency: '94.3',
+    title: 'JavaScript Jabber',
+    description:
+      'Discussão semanal em podcast sobre Javascript na parte frontal e traseira. Discuta também práticas de programação, ambientes de codificação e as comunidades relacionadas à tecnologia',
+    img: '../img/5.png',
+    url: 'https://devchat.cachefly.net/javascriptjabber/JSJ_440_Mc_Cumskey.mp3',
+  },
+  {
+    frequency: '95.6',
+    title: 'JAMstack Radio',
+    description:
+      'Um programa sobre o JAMstack (JavaScript, APIs, marcação), uma nova maneira de criar aplicativos ou sites rápidos e seguros. Trazido a você por Heavybit.',
+    img: '../img/6.jpg',
+    url: 'https://cdn.changelog.com/uploads/jsparty/111/js-party-111.mp3',
+  },
+  {
+    frequency: '97',
+    title: 'JavaScript - Software Engineering Daily',
+    description:
+      'O Software Engineering Daily apresenta entrevistas diárias sobre tópicos técnicos de software.',
+    img: '../img/7.jpg',
+    url: 'https://hwcdn.libsyn.com/p/2/f/b/2fbedb9e599c46cf/2019_11_11_Gatsby.mp3?c_id=57154322&cs_id=57154322&expiration=1595232578&hwt=69f8fb148c79573b610d74524d87fa4e3',
+  },
+  {
+    frequency: '97.2',
+    title: 'JS Party',
+    description:
+      'Uma celebração comunitária do JavaScript e da web. Este programa grava ao vivo às quintas-feiras às 13h, horário dos EUA / Leste. Os tópicos discutidos incluem plataformas da Web, estruturas de front-end, Node.js, animação da Web, SVG, robótica, IoT e muito mais.',
+    img: '../img/8.png',
+    url: 'https://cdn.changelog.com/uploads/jsparty/111/js-party-111.mp3',
+  },
+  {
+    frequency: '98.4',
+    title: 'React Podcast',
+    description:'Conversas sobre o React com seus desenvolvedores favoritos.',
+    img: '../img/9.jpg',
+    url: 'https://cdn.simplecast.com/audio/bdb43d/bdb43d4d-bd1d-4fbc-bd60-40f1e3299aa3/d8f17f95-3f7b-452f-99d4-8b668811b65d/82-becca-bailey_tc.mp3',
+  },
+  {
+    frequency: '99.5',
+    title: 'The Official Vue News',
+    description:
+      'Cobrimos as últimas notícias e tutoriais do Vue em cerca de 5 minutos. Nosso objetivo é fornecer as informações necessárias para você ficar em dia com o seu jogo como desenvolvedor do Vue.',
+    img: '../img/10.jpg',
+    url: 'https://aphid.fireside.fm/d/1437767933/a16d96ec-7315-4f65-b85c-41ace3e84c7e/5c9a8e27-d580-45a9-bb5b-e7f5e2ed1656.mp3',
+  }
+];
---
+++ js/script.js
@@ -0,0 +1,102 @@
+const radioRange = document.querySelector('#rangeRadio');
+const frequencyRadio = document.querySelector('#frequencyRadio');
+const displayPodcast = document.querySelector('#displayPodcast');
+const buttonUp = document.querySelector('#up');
+const buttonDown = document.querySelector('#down');
+
+const img = document.createElement('img');
+img.setAttribute('id', 'image');
+const description = document.createElement('p');
+const audio = document.createElement('audio');
+
+const message = document.createElement('p');
+message.setAttribute('id', 'message');
+message.textContent = 'Sem estação na frequência';
+
+const imgNotFound = document.createElement('img');
+imgNotFound.src = '../img/not_found.png';
+imgNotFound.title = 'Não encontrado';
+imgNotFound.alt = 'Não Encontrado';
+
+function start() {
+  console.log('DOM carregado!');
+  frequencyRadio.textContent = `${radioRange.value} Mhz`;
+  displayPodcast.appendChild(message);
+  displayPodcast.appendChild(imgNotFound);  
+  radioRange.addEventListener('input', handlerRangeValueChange);
+
+  buttonDown.addEventListener('click', () =>{
+    if (radioRange.value > 87.5){
+      radioRange.value = parseFloat(radioRange.value) - 0.1;
+      frequencyRadio.textContent = `${radioRange.value} Mhz`;
+      findPodcastFrom(radioRange.value);
+    }
+  });
+
+  buttonUp.addEventListener('click', () =>{
+    if (radioRange.value < 108){
+      radioRange.value = parseFloat(radioRange.value) + 0.1;
+      frequencyRadio.textContent = `${radioRange.value} Mhz`;
+      findPodcastFrom(radioRange.value);
+    }
+  });
+}
+
+
+function handlerRangeValueChange(event) {
+  const { value } = event.target;
+  frequencyRadio.textContent = `${value} Mhz`;
+  findPodcastFrom(value);
+}
+
+function findPodcastFrom(frequency) {
+  let foundPodcast = null;
+  for (let i = 0; i < realPodcasts.length; i++) {
+    let currentPodCast = realPodcasts[i];
+
+    if (currentPodCast.frequency === frequency) {
+      foundPodcast = currentPodCast;
+      break;
+    }
+  }
+
+  if (!!foundPodcast) {
+    renderFound(foundPodcast);
+  } else {
+    rederNotFound();
+  }
+}
+
+function renderFound(foundPodcast) {
+  img.src = foundPodcast.img;
+  img.alt = foundPodcast.title;
+  img.title = foundPodcast.title;
+
+  description.innerHTML = foundPodcast.description;
+
+  audio.src = foundPodcast.url;
+  audio.autoplay = false;
+  audio.controls = true;
+
+  displayPodcast.removeChild(message);
+  displayPodcast.removeChild(imgNotFound);
+  displayPodcast.appendChild(img);
+  displayPodcast.appendChild(description);
+  displayPodcast.appendChild(audio);
+}
+
+function rederNotFound(){
+  if (displayPodcast.childNodes.length === 2) {
+    displayPodcast.appendChild(message);
+    displayPodcast.appendChild(imgNotFound);
+  } else {
+    displayPodcast.removeChild(img);
+    displayPodcast.removeChild(description);
+    displayPodcast.removeChild(audio);
+    displayPodcast.appendChild(message);
+    displayPodcast.appendChild(imgNotFound);  
+  }
+}
+
+
+start();
